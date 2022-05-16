b75e0e5d5e7a5e1397bea18788f567cedb68b893
Johnny Chen <johnnyc1314@gmail.com>
2022-05-16 13:14:53 -0400

    commit tree 4d8813f1988d293d697e722f8284d1a11ef55d8d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map {|word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.find {|word| word.length> 4} != nil
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find {|snack| cheese_types.include?(snack)}
 end
+
+# roll_call_dwarves(['a', 'betty', 'Charles'])
+# puts summon_captain_planet(['a', 'betty', 'Charles'])
+# puts long_planeteer_calls(['a', 'bett', 'Chars'])
+puts find_the_cheese(['agouda', 'cheddar', 'Chars'])
